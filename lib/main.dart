f583f9f631f84fc37cc4473cd901d99eb8d1d095
Johannes Løvold Josefsen <johannlj@stud.ntnu.no>
2022-01-18 12:14:10 +0100

    Started working on setting theme for app.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,54 @@ void main() {
   runApp(const MyApp());
 }
 
+/*----------------------
+Theme configurations:
+ ----------------------*/
+ThemeData theme = ThemeData(
+  primaryColor: const Color(0xff13293d),
+  colorScheme: colorScheme,
+  scaffoldBackgroundColor: const Color(0xffe8f1f2),
+  appBarTheme: appBarTheme,
+  buttonTheme: buttonThemeData,
+  textTheme: textTheme,
+);
+
+ColorScheme colorScheme = const ColorScheme(
+    primary: Color(0xff13293d),
+    primaryVariant: Color(0xff006494),
+    secondary: Color(0xff1b98e0),
+    secondaryVariant: Color(0xffe8f1f2),
+    surface: Color(0xffe8f1f2),
+    background: Color(0xffe8f1f2),
+    error: Color(0xffe01a1a),
+    onPrimary: Color(0xffe8f1f2),
+    onSecondary: Color(0xffe8f1f2),
+    onSurface: Color(0xff13293d),
+    onBackground: Color(0xff13293d),
+    onError: Color(0xfff2e8e8),
+    brightness: Brightness.dark
+);
+
+
+TextTheme textTheme = const TextTheme(
+  headline1: TextStyle(color: Color(0xff13293d)),
+  headline2: TextStyle(color: Color(0xff13293d)),
+  headline3: TextStyle(color: Color(0xff13293d)),
+  headline4: TextStyle(color: Color(0xff13293d)),
+  headline5: TextStyle(color: Color(0xff13293d)),
+  headline6: TextStyle(color: Color(0xff13293d)),
+  subtitle1: TextStyle(color: Color(0xff13293d)),
+  subtitle2: TextStyle(color: Color(0xff13293d)),
+  bodyText1: TextStyle(color: Color(0xff13293d)),
+  bodyText2: TextStyle(color: Color(0xff13293d)),
+  caption: TextStyle(color: Color(0xff13293d)),
+  button: TextStyle(color: Color(0xff13293d)),
+  overline: TextStyle(color: Color(0xff13293d)),
+);
+
+AppBarTheme appBarTheme = const AppBarTheme(color: Color(0xff13293d));
+ButtonThemeData buttonThemeData = const ButtonThemeData(buttonColor: Color(0xff1b98e0));
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -11,35 +59,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Ship Organizer',
+      theme: theme,
+      routes: {
+        '/': (BuildContext context) => const MyHomePage(title: 'Home'),
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -52,47 +82,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -109,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
