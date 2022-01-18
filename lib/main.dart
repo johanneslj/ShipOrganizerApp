84e663e1e29166895dcb66fb8542920b416339a7
Hans <hansal@ntnu.no>
2022-01-18 11:47:40 +0100

    Started work on the login view, made input fields and buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ship_organizer_app/login_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -57,7 +58,10 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => (LoginView()) //ForgotPasswordPage())),
+          ));
     });
   }
 
