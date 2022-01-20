8d3cb03c598f95cdefd1a09eae3b40ad417cf831
Hans <hansal@ntnu.no>
2022-01-20 09:33:55 +0100

    Made cards for select department

--- lib/views/select_department/select_department_view.dart
+++ lib/views/select_department/select_department_view.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:ship_organizer_app/views/select_department/department_card.dart';
 import '../../../main.dart';
 
 class SelectDepartmentView extends StatelessWidget {
-  const SelectDepartmentView({
+  SelectDepartmentView({
     Key? key,
   }) : super(key: key);
+
+  final List<String> departments = <String>["heia", "Kult", "Sjallabis"];
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-        backgroundColor: const Color(0xff13293D),
         body: Center(
-          child: Column(children: [
+      child: Column(children: [
 
-          ],),
-        ));
+        Column(
+          children: getDepartments(departments),
+        )
+      ]),
+    ));
+  }
+
+  List<Widget> getDepartments(List<String> departments) {
+    List<Widget> departmentCardList = <Widget>[];
+
+    for (String department in departments) {
+      Widget departmentCard = DepartmentCard(
+        departmentName: department,
+      );
+      departmentCardList.add(departmentCard);
+    }
+
+    return departmentCardList;
   }
 }
