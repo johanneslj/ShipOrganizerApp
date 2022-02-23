f09211d28b902cb41a9a42fddce39862d097a502
Simon Duggal <simondu@stud.ntnu.no>
2022-02-23 14:45:36 +0100

    Created api call for get products and get recommended products
    Signed-off-by: Simon Duggal <simondu@stud.ntnu.no>

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,11 +1,92 @@
 import 'dart:core';
+import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:ship_organizer_app/entities/report.dart';
+import 'package:ship_organizer_app/views/inventory/item.dart';
 
 class ApiService {
   final Dio dio = Dio();
-  String baseUrl = "http://10.22.185.131:8080/";
+  String baseUrl = "http://127.0.0.1:8080/";
+
+  ///Gets all products from the backend server
+  ///Returns a list of all the products
+  Future<List<Item>> getItems() async {
+    dio.options.headers["Authorization"] = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJuYW1lIjoiSGFucyBMaW5kZ2FhcmQiLCJpZCI6MjgsImV4cCI6MTY0NzQyMzQ4NCwiZW1haWwiOiJoYW5zYWxAc3R1ZC5udG51Lm5vIn0.TWheVKzg80VL8uH_CxvuFReZOiepRoIyzcfRhmq8BgFuaX6C7d8B21BecGTqVA9Q8Osy1pHZDD0lZoc5kK2TGA";
+    var response = await dio.get(baseUrl + "product/inventory");
+    List<Item> items = [];
+    List<dynamic> products = List<dynamic>.from(response.data);
+    for (var product in products) {
+      String name = "";
+      String number = "";
+      String ean13 = "";
+      int stock = 0;
+      product.forEach((key,value) {
+        switch (key) {
+          case "barcode":
+            ean13 = value;
+            break;
+          case "productName":
+            name = value;
+            break;
+          case "productNumber":
+            number = value;
+            break;
+          case "stock":
+            stock = int.parse(value);
+            break;
+      }
+      });
+      items.add(Item(name:name,productNumber: number,ean13: ean13, amount: stock));
+
+    }
+
+    return items;
+  }
+  ///Gets all products for the recommended inventory report
+  ///Returns list of all products that needs to be refilled
+  Future<List<Item>> getRecommendedItems() async {
+    dio.options.headers["Authorization"] = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJuYW1lIjoiSGFucyBMaW5kZ2FhcmQiLCJpZCI6MjgsImV4cCI6MTY0NzQyMzQ4NCwiZW1haWwiOiJoYW5zYWxAc3R1ZC5udG51Lm5vIn0.TWheVKzg80VL8uH_CxvuFReZOiepRoIyzcfRhmq8BgFuaX6C7d8B21BecGTqVA9Q8Osy1pHZDD0lZoc5kK2TGA";
+    var response = await dio.get(baseUrl + "product/PreferredInventory");
+    List<Item> items = [];
+    List<dynamic> products = List<dynamic>.from(response.data);
+    for (var product in products) {
+      String name = "";
+      String number = "";
+      String ean13 = "";
+      int stock = 0;
+      product.forEach((key,value) {
+        switch (key) {
+          case "barcode":
+            ean13 = value;
+            break;
+          case "productName":
+            name = value;
+            break;
+          case "productNumber":
+            number = value;
+            break;
+          case "stock":
+            stock = int.parse(value);
+            break;
+        }
+      });
+      items.add(Item(name:name,productNumber: number,ean13: ean13, amount: stock));
+
+    }
+
+    return items;
+  }
+
+  /// Update stock for a specific product
+  Future<void> updateStock() async{
+    dio.options.headers["Authorization"] = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJuYW1lIjoiSGFucyBMaW5kZ2FhcmQiLCJpZCI6MjgsImV4cCI6MTY0NzQyMzQ4NCwiZW1haWwiOiJoYW5zYWxAc3R1ZC5udG51Lm5vIn0.TWheVKzg80VL8uH_CxvuFReZOiepRoIyzcfRhmq8BgFuaX6C7d8B21BecGTqVA9Q8Osy1pHZDD0lZoc5kK2TGA";
+    var response = await dio.get(baseUrl + "product/inventory");
+
+
+  }
+
+
 
   /// Gets all the markers from the api
   /// returns a Map with LatLng as keys and lists of reports
