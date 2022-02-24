c49f39e951f92075b157797fd91b8145b467cf03
Simon Duggal <simondu@stud.ntnu.no>
2022-02-24 13:56:18 +0100

    Added test connection method to check if the api server is running
    Signed-off-by: Simon Duggal <simondu@stud.ntnu.no>

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -7,94 +7,115 @@ import 'package:ship_organizer_app/views/inventory/item.dart';
 
 class ApiService {
   final Dio dio = Dio();
-  String baseUrl = "http://172.20.10.2:8080/";
-  var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJuYW1lIjoiU2ltb24gRHVnZ2FsIiwiaWQiOjMxLCJleHAiOjE2NDc1MDk4MzMsImVtYWlsIjoic2ltb25kdUBudG51Lm5vIn0.JO3XVtbhW7lNOWSKcWlnK8_o1zBvPxOmgfeDUHLbVdvs8w40mWqrUT6fkNM2D7iS9LXYbJUm8bC5ImARerkqPg";
+  String baseUrl = "http://127.0.0.1:8080/";
+  var token =
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJuYW1lIjoiU2ltb24gRHVnZ2FsIiwiaWQiOjMxLCJleHAiOjE2NDc1MDk4MzMsImVtYWlsIjoic2ltb25kdUBudG51Lm5vIn0.JO3XVtbhW7lNOWSKcWlnK8_o1zBvPxOmgfeDUHLbVdvs8w40mWqrUT6fkNM2D7iS9LXYbJUm8bC5ImARerkqPg";
+
+  ///Test connection to api server
+  Future<int?> testConnection() async {
+    var testConnection = await dio.get(baseUrl + "actuator/health");
+    return testConnection.statusCode;
+  }
+
   ///Gets all products from the backend server
   ///Returns a list of all the products
   Future<List<Item>> getItems() async {
+    int? connectionCode = await testConnection();
     dio.options.headers["Authorization"] = "Bearer $token";
-    var response = await dio.get(baseUrl + "product/inventory");
     List<Item> items = [];
-    List<dynamic> products = List<dynamic>.from(response.data);
-    for (var product in products) {
-      String name = "";
-      String number = "";
-      String ean13 = "";
-      int stock = 0;
-      product.forEach((key,value) {
-        switch (key) {
-          case "barcode":
-            ean13 = value;
-            break;
-          case "productName":
-            name = value;
-            break;
-          case "productNumber":
-            number = value;
-            break;
-          case "stock":
-            stock = int.parse(value);
-            break;
+    if (connectionCode == 200) {
+      var response = await dio.get(baseUrl + "product/inventory");
+      if (response.statusCode == 200) {
+        List<dynamic> products = List<dynamic>.from(response.data);
+        String name = "";
+        String number = "";
+        String ean13 = "";
+        int stock = 0;
+        for (var product in products) {
+          product.forEach((key, value) {
+            switch (key) {
+              case "barcode":
+                ean13 = value;
+                break;
+              case "productName":
+                name = value;
+                break;
+              case "productNumber":
+                number = value;
+                break;
+              case "stock":
+                stock = int.parse(value);
+                break;
+            }
+          });
+          items.add(Item(
+              name: name, productNumber: number, ean13: ean13, amount: stock));
+        }
       }
-      });
-      items.add(Item(name:name,productNumber: number,ean13: ean13, amount: stock));
-
     }
+
     return items;
   }
+
   ///Gets all products for the recommended inventory report
   ///Returns list of all products that needs to be refilled
   Future<List<Item>> getRecommendedItems() async {
+    int? connectionCode = await testConnection();
     dio.options.headers["Authorization"] = "Bearer $token";
-    var response = await dio.get(baseUrl + "product/RecommendedInventory");
     List<Item> items = [];
-    List<dynamic> products = List<dynamic>.from(response.data);
-    for (var product in products) {
-      String name = "";
-      String number = "";
-      String ean13 = "";
-      int stock = 0;
-      product.forEach((key,value) {
-        switch (key) {
-          case "barcode":
-            ean13 = value;
-            break;
-          case "productName":
-            name = value;
-            break;
-          case "productNumber":
-            number = value;
-            break;
-          case "stock":
-            stock = int.parse(value);
-            break;
-        }
-      });
-      items.add(Item(name:name,productNumber: number,ean13: ean13, amount: stock));
 
+    if (connectionCode == 200) {
+      var response = await dio.get(baseUrl + "product/RecommendedInventory");
+
+      if (response.statusCode == 200) {
+        List<dynamic> products = List<dynamic>.from(response.data);
+        for (var product in products) {
+          String name = "";
+          String number = "";
+          String ean13 = "";
+          int stock = 0;
+          product.forEach((key, value) {
+            switch (key) {
+              case "barcode":
+                ean13 = value;
+                break;
+              case "productName":
+                name = value;
+                break;
+              case "productNumber":
+                number = value;
+                break;
+              case "stock":
+                stock = int.parse(value);
+                break;
+            }
+          });
+          items.add(Item(
+              name: name, productNumber: number, ean13: ean13, amount: stock));
+        }
+      }
     }
 
     return items;
   }
 
   /// Update stock for a specific product
-  Future<void> updateStock(String productnumber,String username,int amount,double latitude,double longitude) async{
+  Future<void> updateStock(String productnumber, String username, int amount,
+      double latitude, double longitude) async {
+    int? connectionCode = await testConnection();
+
     dio.options.headers["Authorization"] = "Bearer $token";
 
-    var response = await dio.post(baseUrl + "product/setNewStock",data:{
-      "productnumber": productnumber,
-      "username": username,
-      "quantity": amount,
-      "latitude": latitude,
-      "longitude": longitude
-    });
-
-    var result =  response;
-
-
+    if (connectionCode == 200) {
+      await dio.post(baseUrl + "product/setNewStock", data: {
+        "productnumber": productnumber,
+        "username": username,
+        "quantity": amount,
+        "latitude": latitude,
+        "longitude": longitude
+      });
+    }
   }
-
-
 
   /// Gets all the markers from the api
   /// returns a Map with LatLng as keys and lists of reports
@@ -129,7 +150,8 @@ class ApiService {
           /// First an empty Report is created then each of its fields
           /// are set sequentially until all of them have a value
           Report reportFromData = Report();
-          Map<String, dynamic>.from(report).forEach((identifier, reportFieldValue) {
+          Map<String, dynamic>.from(report)
+              .forEach((identifier, reportFieldValue) {
             switch (identifier) {
               case "productName":
                 reportFromData.setName(reportFieldValue);
@@ -144,7 +166,8 @@ class ApiService {
                 reportFromData.setLongitude(reportFieldValue);
                 break;
               case "registrationDate":
-                reportFromData.setDate(DateTime.parse(reportFieldValue.split(".")[0]));
+                reportFromData
+                    .setDate(DateTime.parse(reportFieldValue.split(".")[0]));
                 break;
               case "fullName":
                 reportFromData.setUserName(reportFieldValue);
@@ -155,7 +178,8 @@ class ApiService {
         }
         double latitude = double.parse(key.split(", ")[0]);
         double longitude = double.parse(key.split(", ")[1]);
-        reports.putIfAbsent(LatLng(latitude, longitude), () => reportsOnSameLatLng);
+        reports.putIfAbsent(
+            LatLng(latitude, longitude), () => reportsOnSameLatLng);
       }
     });
     return reports;
