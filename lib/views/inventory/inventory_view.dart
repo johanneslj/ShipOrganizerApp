83f0b41edf5ea91045c895800f272722ce2238dd
Simon Duggal <simondu@stud.ntnu.no>
2022-03-02 11:47:12 +0100

    Added function to start barcode scanner for search
    Signed-off-by: Simon Duggal <simondu@stud.ntnu.no>

--- lib/views/inventory/inventory_view.dart
+++ lib/views/inventory/inventory_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:ship_organizer_app/api%20handling/api_controller.dart';
 import 'package:ship_organizer_app/entities/department.dart';
 import 'package:ship_organizer_app/views/inventory/add_remove_item_dialog.dart';
@@ -65,6 +67,7 @@ class _InventoryViewState extends State<InventoryView> {
               filter: showSelectDepartmentMenu,
               controller: _controller,
               recommended: false,
+              onScan: scanBarcodeNormal,
             )),
         drawer: const SideMenu(),
         body: _isLoading ? circularProgress() : GestureDetector(
@@ -106,6 +109,26 @@ class _InventoryViewState extends State<InventoryView> {
       displayedItems = result;
     });}
 
+
+  ///Method to scan the barcode
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> scanBarcodeNormal() async {
+    String barcodeScanRes;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      barcodeScanRes =
+      await FlutterBarcodeScanner.scanBarcode('#ff6666', 'Cancel', true, ScanMode.BARCODE);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+    setState(() {
+      _controller.text = barcodeScanRes;
+    });
+  }
   /// Displays the select department pop up menu, where the user can select which department's inventory
   /// they want to view.
   void showSelectDepartmentMenu() async {
@@ -117,10 +140,8 @@ class _InventoryViewState extends State<InventoryView> {
 
   /// Gets the departments as a [List] of [PopupMenuItem] to be used in the select department pop up menu.
   Future<List<PopupMenuItem>> getPopupMenuItems() async {
-
     List<String> departments = await apiService.getDepartments();
     List<PopupMenuItem> popMenuItems = [];
-
     for (String department in departments) {
       popMenuItems.add(
         PopupMenuItem(
