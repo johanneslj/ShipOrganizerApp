754e0a70eb55c8363775f3425e17376f5f017fd0
Johannes Løvold Josefsen <johannlj@stud.ntnu.no>
2022-03-29 13:20:44 +0200

    Removed unused import. Small change in set_password appbar.

--- lib/api handling/api_controller.dart
+++ lib/api handling/api_controller.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:core';
-import 'package:connectivity/connectivity.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
