6c739e51f982b0a1ac991e9ecb902a1d1a392162
Hans <hansal@ntnu.no>
2022-05-04 14:39:23 +0200

    Changed some colors

--- lib/views/inventory/inventory_view.dart
+++ lib/views/inventory/inventory_view.dart
@@ -267,19 +267,14 @@ class _InventoryViewState extends State<InventoryView> {
   }
 
   ///Method to scan the barcode
-  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> scanBarcodeNormal() async {
     String barcodeScanRes;
-    // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-          '#ff6666', 'Cancel', true, ScanMode.BARCODE);
+          "e8f1f2", "Cancel", true, ScanMode.BARCODE);
     } on PlatformException {
-      barcodeScanRes = 'Failed to get platform version.';
+      barcodeScanRes = "Failed to get platform version.";
     }
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
     setState(() {
       _controller.text = barcodeScanRes != "-1" ? barcodeScanRes : "";
